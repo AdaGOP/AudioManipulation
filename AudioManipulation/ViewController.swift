9cae6e4d8a9da462c9726d16ae0f00969a05d402
zein rezky chandra <zeinrezkychandra@zeins-MacBook-Pro.local>
2020-06-07 14:19:08 +0700

    create simple mixer

--- AudioManipulation/ViewController.swift
+++ AudioManipulation/ViewController.swift
@@ -20,12 +20,26 @@ class ViewController: UIViewController {
 //    let outputFile = AVAudioFile()
     let delay = AVAudioUnitDelay()
     
-    
+    @IBOutlet weak var delayProgressLevel: UIProgressView!
+    @IBOutlet weak var delayPlusButton: UIButton!
+    @IBOutlet weak var delayMinusButton: UIButton!
+    @IBOutlet weak var reverbProgressLevel: UIProgressView!
+    @IBOutlet weak var reverbPlusButton: UIButton!
+    @IBOutlet weak var reverbMinusButton: UIButton!
+    @IBOutlet weak var distortionProgressLevel: UIProgressView!
+    @IBOutlet weak var distortionPlusButton: UIButton!
+    @IBOutlet weak var distortionMinusButton: UIButton!
+    @IBOutlet weak var speedProgressLevel: UIProgressView!
+    @IBOutlet weak var speedPlusButton: UIButton!
+    @IBOutlet weak var speedMinusButton: UIButton!
+    @IBOutlet weak var pitchProgressLevel: UIProgressView!
+    @IBOutlet weak var pitchPlusButton: UIButton!
+    @IBOutlet weak var pitchMinusButton: UIButton!
     @IBOutlet weak var playButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        playButton.addTarget(self, action: #selector(playAudio), for: .touchUpInside)
+        configAudioButton()
     }
 
     @objc func playAudio() {
@@ -54,10 +68,10 @@ class ViewController: UIViewController {
         // We need to make sure our each component from audio property that we create before such as speed and pitch are not crash each other. Remember concept of the audio is like having a port for input port, and output port, so if we want to play and manipulate the audio based on the node, we have to arrange the parts so that output from one is input to another
         audioEngine.connect(audioPlayer, to: audioSpeedControl, format: nil)
         audioEngine.connect(audioSpeedControl, to: audioPitchControl, format: nil)
-        audioEngine.connect(audioPitchControl, to: distortion, format: nil)
-        audioEngine.connect(distortion, to: reverb, format: nil)
-        audioEngine.connect(reverb, to: delay, format: nil)
-        audioEngine.connect(delay, to: audioEngine.mainMixerNode, format: nil)
+        audioEngine.connect(audioPitchControl, to: reverb, format: nil)
+//        audioEngine.connect(distortion, to: reverb, format: nil)
+        audioEngine.connect(reverb, to: audioEngine.mainMixerNode, format: nil)
+//        audioEngine.connect(delay, to: audioEngine.mainMixerNode, format: nil)
 
         
         audioPlayer.scheduleFile(file, at: nil) // Last we need to prepare the player to play audio sequencially start from beginning to end
@@ -66,13 +80,102 @@ class ViewController: UIViewController {
         audioPlayer.play()
     }
     
-    // Now let's try to manipulate audio whenever view being tapped, by increasing the number of pitch and speed of the audio based on the audio node
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        audioPitchControl.pitch += 10
-        audioSpeedControl.rate += 0.1
-        audioPitchControl.overlap += 10
-        distortion.wetDryMix += 50
-    }
-    
 }
 
+
+// MARK: Audio Control Button
+extension ViewController {
+    func configAudioButton() {
+        playButton.addTarget(self, action: #selector(playAudio), for: .touchUpInside)
+        pitchMinusButton.addTarget(self, action: #selector(pitchMinusAction), for: .touchUpInside)
+        pitchPlusButton.addTarget(self, action: #selector(pitchPlusAction), for: .touchUpInside)
+        speedMinusButton.addTarget(self, action: #selector(speedMinusAction), for: .touchUpInside)
+        speedPlusButton.addTarget(self, action: #selector(speedPlusAction), for: .touchUpInside)
+        distortionMinusButton.addTarget(self, action: #selector(distortMinusAction), for: .touchUpInside)
+        distortionPlusButton.addTarget(self, action: #selector(distortPlusAction), for: .touchUpInside)
+        reverbMinusButton.addTarget(self, action: #selector(reverbMinusAction), for: .touchUpInside)
+        reverbPlusButton.addTarget(self, action: #selector(reverbPlusAction), for: .touchUpInside)
+        delayMinusButton.addTarget(self, action: #selector(delayMinusAction), for: .touchUpInside)
+        delayPlusButton.addTarget(self, action: #selector(delayPlusAction), for: .touchUpInside)
+    }
+    
+    @objc
+    func pitchMinusAction() {
+        audioPitchControl.pitch -= 10
+        DispatchQueue.main.async {
+            self.pitchProgressLevel.progress -= 0.1
+        }
+    }
+    
+    @objc
+    func pitchPlusAction() {
+        audioPitchControl.pitch += 10
+        DispatchQueue.main.async {
+            self.pitchProgressLevel.progress += 0.1
+        }
+    }
+    
+    @objc
+    func speedMinusAction() {
+        audioSpeedControl.rate -= 0.1
+        DispatchQueue.main.async {
+            self.speedProgressLevel.progress -= 0.1
+        }
+    }
+    
+    @objc
+    func speedPlusAction() {
+        audioSpeedControl.rate += 0.1
+        DispatchQueue.main.async {
+            self.speedProgressLevel.progress += 0.1
+        }
+    }
+    
+    @objc
+    func distortMinusAction() {
+        distortion.wetDryMix -= 50
+        DispatchQueue.main.async {
+            self.distortionProgressLevel.progress -= 0.1
+        }
+    }
+    
+    @objc
+    func distortPlusAction() {
+        distortion.wetDryMix += 50
+        DispatchQueue.main.async {
+            self.distortionProgressLevel.progress += 0.1
+        }
+    }
+    
+    @objc
+    func reverbMinusAction() {
+        reverb.wetDryMix -= 50
+        DispatchQueue.main.async {
+            self.reverbProgressLevel.progress -= 0.1
+        }
+    }
+    
+    @objc
+    func reverbPlusAction() {
+        reverb.wetDryMix += 50
+        DispatchQueue.main.async {
+            self.reverbProgressLevel.progress += 0.1
+        }
+    }
+    
+    @objc
+    func delayMinusAction() {
+        delay.feedback -= 50
+        DispatchQueue.main.async {
+            self.delayProgressLevel.progress -= 0.1
+        }
+    }
+    
+    @objc
+    func delayPlusAction() {
+        delay.feedback += 50
+        DispatchQueue.main.async {
+            self.delayProgressLevel.progress += 0.1
+        }
+    }
+}
